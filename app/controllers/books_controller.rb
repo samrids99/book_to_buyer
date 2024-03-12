b57e1649f3b0177b3a477f15f0be528aa830bc78
Sam Ridsdale <samridsdale108@gmail.com>
2024-03-12 15:09:56 +0000

    Add dotenv-rails

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,3 +3,37 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 end
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+  before_action :authenticate_user!, only: [:create]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+      raise @book.errors.full_messages.inspect
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :author, :cover_image, :description)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_one_attached :cover_image
   belongs_to :user
   validates :title, presence: true
   validates :genre, presence: true
