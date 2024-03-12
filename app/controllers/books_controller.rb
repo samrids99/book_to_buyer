f3fd7a48504ad8d5534a71d3313f176defa11204
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-12 15:23:09 +0000

    completed as a user I can destroy a book and partially completed search for a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,29 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show destroy]
+  before_action :authorize_user!, only: :destroy
+
   def index
+    if params[:search]
+      @books = Book.where("title LIKE ?", "%#{params[:search]}%")
+    else
+      @books = Book.all
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully deleted.'
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def authorize_user!
+    unless current_user == @book.user
+      redirect_to books_path, alert: "You are not authorized to delete this book."
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_111804) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_131039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_111804) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
