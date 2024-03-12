a75bb6b16fad985ae356381a9a61c7b7eabda50a
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-12 13:39:33 +0000

    added seeds to books and created books path

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.destroy_all
+User.destroy_all
+
+user_data = [
+  {username: "d4566", password: "123456", email: "user1@email.com", location: "Uk"}
+]
+User.create!(user_data)
+
+
+
+# Clear existing records to avoid duplication when running 'rails db:seed' multiple times
+
+
+# Create seed data for books
+books_data = [
+  { user: User.first, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', genre: 'Classic', description: 'A novel about the American Dream.' },
+  { user: User.first, title: 'To Kill a Mockingbird', author: 'Harper Lee', genre: 'Fiction', description: 'A story of racial injustice and moral growth.' },
+  { user: User.first, title: '1984', author: 'George Orwell', genre: 'Dystopian', description: 'A dystopian novel exploring totalitarianism.' }
+]
+
+books_data.each do |book_data|
+  Book.create!(book_data)
+end
+
+puts 'Seed data for books created successfully!'
