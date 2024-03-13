b7d8fae5811b1e81054b503e6dfdae952260ca84
Jack Richmond <jacksrichmond@yahoo.co.uk>
2024-03-13 11:16:30 +0000

    Your local changes to the following files would be overwritten by checkout: 	app/controllers/books_controller.rb 	app/views/layouts/application.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,59 +1,39 @@
-
 class BooksController < ApplicationController
-
-  before_action :set_book, only: [:show]
-  before_action :authenticate_user!, only: [:create]
-
-
+  before_action :set_book, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
   def index
-    if params[:search]
+    if params[:search].present?
       @books = Book.where("title LIKE ?", "%#{params[:search]}%")
     else
       @books = Book.all
     end
   end
-
-  def destroy
-    @book.destroy
-    redirect_to books_path, notice: 'Book was successfully deleted.'
-  end
-
-  private
-
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  def authorize_user!
-    unless current_user == @book.user
-      redirect_to books_path, alert: "You are not authorized to delete this book."
-    end
-  end
-
   def show
   end
-
   def new
-    @book = Book.new
+    @book = current_user.books.new
   end
-
   def create
     @book = current_user.books.new(book_params)
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
       render :new
-      raise @book.errors.full_messages.inspect
     end
   end
-
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :genre, :author, :cover_image, :description)
+  def destroy
+    if current_user == @book.user
+      @book.destroy
+      redirect_to books_path, notice: 'Book was successfully deleted.'
+    else
+      redirect_to books_path, alert: "You are not authorized to delete this book."
+    end
   end
-
+  private
   def set_book
     @book = Book.find(params[:id])
+  end
+  def book_params
+    params.require(:book).permit(:title, :genre, :author, :cover_image, :description)
   end
 end
