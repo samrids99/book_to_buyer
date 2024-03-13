441ac6c8b5aec123d93722833df5fd76ee9c74cd
Jack Richmond <jacksrichmond@yahoo.co.uk>
2024-03-13 16:12:23 +0000

    gemfile problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+# app/controllers/pages_controller.rb
+require_relative '../../app/services/google_books_service'
+
 class PagesController < ApplicationController
   def home
     @books = GoogleBooksService.fetch_trending_books(20)
