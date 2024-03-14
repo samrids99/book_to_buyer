1946ea729c7ef9320687526164256b18f1343f4d
Sam Ridsdale <samridsdale108@gmail.com>
2024-03-14 14:25:37 +0000

    datepicker fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :location, presence: true
   has_many :bookings
   has_many :books
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
