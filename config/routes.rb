44537c335a35d887656d5ba72bb2456ea3b908c9
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-15 11:46:24 +0000

    added so only user that posted thebook can approve and to show

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  #resources :users, only: [:new, :create]
+  #resources :sessions, only: [:new, :create, :destroy]
 
   resources :books do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :show]
 
   # Add a route for the home page
   get 'home', to: 'home#index', as: 'home'
-<<<<<<< HEAD
   get '/bookings/:id/accept', to: 'bookings#approve_booking', as: 'accept'
   get '/bookings/:id/decline', to: 'bookings#decline_booking', as: 'decline'
-
-=======
->>>>>>> ac5e4b24a5b1c4454bc5a09e7e0f0d15180a2948
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
